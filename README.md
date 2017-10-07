dc1c84897ed93f4f42c2c6d8ce2c50e6e70384f4
YOUR NAME <YOU@EXAMPLE.COM>
2017-10-07 09:06:14 +0800

    第一个python应用

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # django_webapp
 利用django创建web项目
-python manage.py startapp cmdb
+python manage.py startapp cmdb(cmdb为web名称)
+
+创建表结构
+python manage.py makemigrations cmdb
--- django_webapp/cmdb/views.py
+++ django_webapp/cmdb/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-from django.shortcuts import HttpResponse
+from cmdb import models
 # Create your views here.
 
+user_list = [
+    {"user":"jack","pwd":"abc"},
+    {"user":"tom","pwd":"ABC"}
+]
+
 def index(request):
-    return HttpResponse("Hello World!")
+    if request.method == "POST":
+        username = request.POST.get("username",None)
+        password = request.POST.get("password",None)
+        global user_list
+        # 将数据保存在数据库
+        models.UserInfo.objects.create(user = username,pwd = password)
+
+        # 从数据库中数据
+        user_list = models.UserInfo.objects.all()
+
+        print(username,password)
+    return render(request,"index.html",{"data":user_list},None,200)
